fb17dca9329548a47650aba6ef4dd71dcb21e70d
Orthoceras <florian.berthier@hotmail.com>
2023-01-03 10:04:33 +0100

    feat: creation page account

---
+++ src/account/hooks/useAccount.jsx
@@ -0,0 +1,5 @@
+export const useAccount = {
+    first_name: "Prénom",
+    last_name: "Nom",
+    email: "Mail",
+  }
--- src/account/pages/Account.jsx
+++ src/account/pages/Account.jsx
@@ -1,8 +1,56 @@
+import { useEffect } from "react";
+import { call, showDatas } from "../../shared/stores";
+import { useDispatch, useSelector } from "react-redux";
+import { CircularProgress, List } from "@mui/material";
+import { useAccount } from "../hooks/useAccount";
+
 export const Account = () => {
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    const getAllInfos = async () => {
+      dispatch(call("users/me", [], "get", "", "me"));
+    };
+    return getAllInfos;
+  }, []);
   
+  let { data, isLoading, error } = useSelector(showDatas);
+
+  const hasDatas = !isLoading && data.me;
+
+  console.log(data.me)
+
   return (
     <>
       <h1>Page Account</h1>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "10vw",
+          }}
+        >
+          <CircularProgress color="inherit" />
+        </div>
+      ) : hasDatas ? (
+        <div className="list">
+          {data.modules.map(function (object, key) {
+            return (
+              <List
+                displayList={object}
+                typeList={useAccount}
+                key={key}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <h2 style={{ textAlign: "center" }}>
+          Vous n'êtes actuellement pas connecté
+        </h2>
+      )}
     </>
   );
 };
