7732207e67bb8a83f2b1c8156fb53118de3bc73e
MatthiasChometon <matthiaschometon787@gmail.com>
2023-01-03 17:30:26 +0100

    feat: move hooks in AppView

--- src/app/atoms/AppView.jsx
+++ src/app/atoms/AppView.jsx
@@ -1,9 +1,15 @@
 import { useAuthenticationMiddleware } from "../../authentication/shared/hooks/useAuthenticationMiddleware"
 import { Appbar } from "../../shared/components/molecules/Appbar/Appbar"
 import { Routing } from "../../shared/router/Routing"
+import { useModule } from "../../teacher/moduleList/hooks/useModuleList"
+import { useStudent } from "../../teacher/studentList/hooks/useStudentList"
+import { useRefreshConnectedUser } from "../../authentication/login/hooks/useRefreshConnectedUser"
 
 export const AppView = () => {
   useAuthenticationMiddleware()
+  useRefreshConnectedUser();
+  useStudent();
+  useModule();
 
   return (
     <>
